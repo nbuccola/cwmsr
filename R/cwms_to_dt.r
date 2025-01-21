83aa2ea554ee08d2bcb5601710346aab34c0a8f0
Norman Buccola <59403625+nbuccola@users.noreply.github.com>
2025-01-21 14:39:26 -0800

    Add files via upload

---
+++ R/cwms_to_dt.r
@@ -0,0 +1,60 @@
+#' Function to get CWMS data from one path on the USACE CDA
+#' Written by NBuccola on 2025-01-21 based on Jetilton; taken from https://github.com/jetilton/cwms_read/blob/master/cwms_read.r
+#'
+#' @param path character; CWMS paths to read data from
+#' @param start_date character; The begin date of extraction, in 'YYYY-MM-DD HH:MM' format
+#' @param end_date character; The end date of extraction, in 'YYYY-MM-DD HH:MM' format
+#' @param timezone character; Example is 'PST8PDT' or 'UTC'
+#' @param timeseries logical; Use "F" or "FALSE" if retrieving location-level data; some types of location levels not setup yet
+#' @param CDApath = character; default to 'https://wm.nww.ds.usace.army.mil:8243/nwdp-data/'; untested for other districts...
+#' @param linux = logical; if running on LINUX OS, use "T"
+#' @return a data.frame of the data
+#' @author Norman Buccola
+#' @keywords CWMS Corps USACE data retrieval
+#' @examples
+#' @export
+cwms_to_dt<-function(path, start_date, end_date, timezone = timezone,
+                     timeseries,CDApath = CDApath,linux=F){
+  library(jsonlite)
+  url = get_url(path, start_date, end_date, timezone = timezone,timeseries)
+  print(paste(paste('`',path,sep=''),'`',sep=''))
+  print(url)
+  if(linux){
+    # For Linux with json
+    download.file(url, destfile = "Station_dataFile.json", method = "libcurl",
+                  extra = "--no-check-certificate",headers =c(a = 'accept: application/json;version=2' ))
+
+    #httpResponse <- GET(url,add_headers(a = 'accept:', b = 'application/json;version=2'), accept_json())
+    # data <- rjson::fromJSON(file = content(httpResponse, "text"))
+    # data <- jsonlite::fromJSON(content(httpResponse, "text"))
+    # download.file(url, destfile = "Station_dataFile.json", method = "wget", extra = "--no-check-certificate")
+    #data = rjson::fromJSON(file="Station_dataFile.json")
+    data = jsonlite::fromJSON("Station_dataFile.json")
+  }else{
+    # For Windows machine
+    data = jsonlite::fromJSON(url)
+  }
+  if(timeseries){
+    if(length(data$`values`)>0){
+
+      vals <- data.frame(data$`values`)
+      nms <- data$`value-columns`$`name`
+      colnames(vals) <- nms
+      dt = data.frame(Date = as.POSIXct(vals$`date-time`/1000,tz =timezone),
+                      value = as.numeric(vals$value),stringsAsFactors = F)
+    }else{
+      dt = data.frame(Date = as.POSIXct(c(start_date,end_date),tz =timezone),value = NA)
+    }
+  }else{ # Location Levels
+    if(length(data$values)>0){
+      dat <- data$values
+      nms <- data$name
+      dt = data.frame(Date = as.POSIXct(dat[,1]/1000,tz =timezone),
+                      value = round(dat[,2],1),stringsAsFactors = F)
+     }else{
+      dt = data.frame(Date = as.POSIXct(c(start_date,end_date),tz =timezone),value = NA)
+    }
+  }
+  colnames(dt) <- c('Date', path)
+  return(dt)
+}
---
+++ R/get_url.r
@@ -0,0 +1,44 @@
+#' Function to create full URL for cwms_get.r
+#' Written by NBuccola on 2025-01-21 based on Jetilton; taken from https://github.com/jetilton/cwms_read/blob/master/cwms_read.r
+#'
+#' @param path character; CWMS paths to read data from
+#' @param start_date character; The begin date of extraction, in 'YYYY-MM-DD HH:MM' format
+#' @param end_date character; The end date of extraction, in 'YYYY-MM-DD HH:MM' format
+#' @param timezone character; Example is 'PST8PDT' or 'UTC'
+#' @param timeseries logical; Use "F" or "FALSE" if retrieving location-level data; some types of location levels not setup yet
+#' @param CDApath = character; default to 'https://wm.nww.ds.usace.army.mil:8243/nwdp-data/'; untested for other districts...
+#' @return a data.frame of the data
+#' @author Norman Buccola
+#' @keywords CWMS Corps USACE data retrieval
+#' @examples
+#' @export
+get_url<-function(path, start_date, end_date, timezone = 'PST8PDT',
+                  timeseries=T,
+                  CDApath = 'https://wm.nww.ds.usace.army.mil:8243/nwdp-data/'){
+  start_date = format(strptime(start_date,"%Y-%m-%d"),"%Y-%m-%dT%H:%M:%S")
+  end_date = format(strptime(end_date, "%Y-%m-%d"),"%Y-%m-%dT%H:%M:%S")
+  office = '&office=NWDP'
+  begin <-  paste0('&begin=',start_date)
+  end <- paste0('&end=',end_date)
+  pageSize <- '&page-size=50000'
+
+  if(timeseries){
+    dscrptr <- 'timeseries?'
+    path <- paste0('name=',path)
+    #unit = "&unit=EN"
+    url = paste0(CDApath,dscrptr,path,office,begin,end,#unit,
+                 paste0('&timezone=',timezone),
+                 pageSize) # Defaults to 500
+  }else{
+    dscrptr <- 'levels/'
+    dscrptr2 <- '/timeseries?'
+    pageSize <- ''
+    unit = "&unit=EN"
+    url = paste0(CDApath,dscrptr,path,dscrptr2,office,begin,end,#
+                 paste0('&timezone=',timezone),
+                 pageSize,unit) # Defaults to 500
+
+  }
+  url = URLencode(url)
+  return(url)
+}
