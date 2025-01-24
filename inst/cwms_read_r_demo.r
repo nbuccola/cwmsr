fbbf6300162b722cc7415749eef7a70a516e411e
Norman Buccola <norman.buccola@usace.army.mil>
2025-01-23 17:50:35 -0800

    Cleaned up graphic labels and legend

--- inst/cwms_read_r_demo.r
+++ inst/cwms_read_r_demo.r
@@ -4,7 +4,8 @@ library(devtools)
 install_github("nbuccola/cwmsr")
 install.packages('cwmsr')
 library(cwmsr)
-LocalWd <- 'C:/Users/g2echnb9/OneDrive - US Army Corps of Engineers/Desktop'
+library(foreach)
+LocalWd <- ''
 CDApath <- 'https://wm.nww.ds.usace.army.mil:8243/nwdp-data/'
 # Setup input variables
 beginDate <- strptime('2024-01-01',"%Y-%m-%d",tz = 'PST8PDT')
@@ -14,39 +15,41 @@ CWMSpaths <- c('DET.Elev-RuleCurve.Inst.~1Day.0.CENWP-CALC',
                'DET.Flow-Gen.Ave.1Hour.1Hour.Best',
                'DET.Flow-Spill.Ave.1Hour.1Hour.Best',
                'BCL.Flow-Out.Ave.1Hour.1Hour.Best',
-               'BCLO.Temp-Water.Inst.0.0.Best') 
-# Access CWMS data via CDA (CWMS Data Acquisition) 
+               'BCLO.Temp-Water.Inst.0.0.Best')
+# Access CWMS data via CDA (CWMS Data Acquisition)
 x24 <- get_cwms(CWMSpaths,start_date=beginDate,end_date=endDate,
                 CDApath=CDApath)
 
+x = x24
 library(dplyr)
 library(tidyr)
 cwmsPivot_longer <- function(x){
-  x |> 
+  x |>
     tidyr::pivot_longer(-Date) |>
-    tidyr::separate(name, c("Site", "D1","D2","AvgPeriod","D4","D5"), sep = "\\.") |>
-    dplyr::select(-D4,-D5) |> 
-    dplyr::mutate(D1a = D1) |>
-    tidyr::separate(D1, c("D1b", "D1c"), sep = "\\-") 
+    mutate(name = gsub('.Ave|.1Hour|.CENWP-CALC|.~1Day|.Best|.Inst|.0|Best','',name)) |>
+    tidyr::separate(name, c("Site", "D1"), sep = "\\.") |>
+    tidyr::separate(D1, c("D1a", "D1b"), sep = "\\-")
 }
-  
+
 x = cwmsPivot_longer(x24) |>
 # Reformat factors for plotting in GGplot
   dplyr::mutate(Site = factor(Site,levels = c('DET','BCL','BCLO'),ordered=T),
-                D1a = as.factor(D1a),
-                D1b = as.factor(gsub('Flow','Flow[cfs]',
-                                     gsub('Elev','Elev[ft]',
-                                          gsub('Temp','Temp[DegF]',D1b)))),
-                D1c = as.factor(D1c),
-                D2 = as.factor(D2))
+                D1c = as.factor(paste(Site,D1a,D1b,sep = '-')),
+                D1a = as.factor(gsub('Flow','Flow [cfs]',
+                                     gsub('Elev','Elev [ft]',
+                                          gsub('Temp','Temp [DegF]',D1a)))),
+                D1b = as.factor(D1b)
+                )
 figTitle <- "DET-BCL_2024_Temperature_Operations"
 
+library(ggplot2)
 fig <-
-  ggplot(x,
-         aes(x=Date,y=value,colour=D1a)) +
-  geom_path(alpha=0.6,linewidth = 1) +
-  geom_point(alpha=0.6,size = 0.5) +
-  facet_grid(D1b~.,scales = 'free', switch="y") +
+  ggplot(x |>
+           drop_na(),
+         aes(x=Date,y=value,colour=D1c)) +
+  geom_line(alpha=0.6,linewidth = 1) +
+  #geom_point(alpha=0.6,size = 0.5) +
+  facet_grid(D1a~.,scales = 'free', switch="y") +
   scale_x_datetime(breaks = seq.POSIXt(from = min(x$Date),to = max(x$Date),by = 'month'),
     date_labels = '%b'
   ) +
